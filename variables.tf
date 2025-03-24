7c6483e3a315962b927a2f56fba57aff8a16958e
Venura Athukorala <venura9@users.noreply.github.com>
2025-03-25 01:11:46 +1100

    removed unwanted vars

--- variables.tf
+++ variables.tf
@@ -1,34 +1,3 @@
-variable "terraform_organization" {
-  description = "Terraform Cloud organization name"
-  type        = string
-}
-
-variable "terraform_workspace" {
-  description = "Terraform Cloud workspace name"
-  type        = string
-}
-
-variable "azure_subscription_id" {
-  description = "Azure subscription ID"
-  type        = string
-}
-
-variable "azure_client_id" {
-  description = "Azure client ID"
-  type        = string
-}
-
-variable "azure_client_secret" {
-  description = "Azure client secret"
-  type        = string
-  sensitive   = true
-}
-
-variable "azure_tenant_id" {
-  description = "Azure tenant ID"
-  type        = string
-}
-
 variable "azure_region" {
   description = "Azure region"
   type        = string
